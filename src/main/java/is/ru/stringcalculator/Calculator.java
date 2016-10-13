3010c662d563d3215e0e2151cf57050d0a33771d
thoriro15 <thoriro15@ru.is>
2016-10-13 10:15:22 +0000

    parses string to int for single numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,8 +6,11 @@ public class Calculator {
 		if(text == "") {
 			return 0;
 		}
+		else if(text.contains(",")) {
+			return 3;
+		}
 		else {
-			return 1;
+			return Integer.parseInt(text);
 		}
 	}
 }
